7f83dc8a739d84c0380d2203731cd2bd10ccb3b0
xwomen1 <dohoikute@gmail.com>
2024-03-01 14:22:39 +0700

    l

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import Map from './Map/Map';
-import React, { useState } from 'react';
-import RestaurantList from './RestaurantList/RestaurantList';
+// import React, { useState } from 'react';
+// import RestaurantList from './RestaurantList/RestaurantList';
 
 const apikey = 'ztM1WgNY33qugsdwo08ZIQSRsAlobRop4kAZka7e5Zo'
 
+
 function App() {
   // Austurvöllur square in Reykjavik
-const userPosition = { lat: 21.019852790335754, lng: 105.81759163037341 };
-const restaurantList = [
-  {
-    name: "Chua lang",
-    location: { lat: 21.016387, lng: 105.817611 },
-  },
-  {
-    name: "thanh cong",
-    location: { lat: 21.020053799230134, lng: 105.81831797041525 },
-  },
-  {
-    name: "lang chu tich",
-    location: { lat: 21.038556503867852, lng: 105.83089912105224 },
-  },
-  {
-    name: "doi can",
-    location: { lat: 21.035711982112772, lng: 105.81891629988009 },
-  },
-];
-const [restaurantPosition, setRestaurantPosition] = useState(null);
+ const userPosition = { lat: 21.019852790335754, lng: 105.81759163037341 };
+// const restaurantList = [
+//   {
+//     name: "Chua lang",
+//     location: { lat: 21.016387, lng: 105.817611 },
+//   },
+//   {
+//     name: "thanh cong",
+//     location: { lat: 21.020053799230134, lng: 105.81831797041525 },
+//   },
+//   {
+//     name: "lang chu tich",
+//     location: { lat: 21.038556503867852, lng: 105.83089912105224 },
+//   },
+//   {
+//     name: "doi can",
+//     location: { lat: 21.035711982112772, lng: 105.81891629988009 },
+//   },
+// ];
+// const [restaurantPosition, setRestaurantPosition] = useState(null);
 
-const onClickHandler_ = (location) => {
-  setRestaurantPosition(location);
-};
+// const onClickHandler_ = (location) => {
+//   setRestaurantPosition(location);
+// };
 
   return (
     <div>
-      <RestaurantList list={restaurantList} onClickHandler={onClickHandler_} />
+      {/* <RestaurantList list={restaurantList} onClickHandler={onClickHandler_} /> */}
       <Map
         apikey={apikey}
         userPosition={userPosition}
-        restaurantPosition={restaurantPosition}
+        // restaurantPosition={restaurantPosition}
       />
     </div>
   );
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,35 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import H from '@here/maps-api-for-javascript';
+import { Modal } from '@mui/material';
+import BasicModal from './popup';
 
-const Map = ( props ) => {
+const Map = (props) => {
     const mapRef = useRef(null);
     const map = useRef(null);
-    const platform = useRef(null)
+    const platform = useRef(null);
     const { apikey, userPosition, restaurantPosition } = props;
+    const [open, setopen] = useState(false);
+    const [lat, setLat] = useState(null);
+    const [long, setLong] = useState(null);
+  
 
-    useEffect(
-        
-        () => {
-            if (restaurantPosition) {
-                calculateRoute(platform.current, map.current, userPosition, restaurantPosition);
-            }
-            
-          // Check if the map object has already been created
-          if (!map.current) {
+
+    useEffect(() => {
+        // Check if the map object has already been created
+        if (!map.current) {
             // Create a platform object with the API key
             platform.current = new H.service.Platform({ apikey });
             // Create a new Raster Tile service instance
             const rasterTileService = platform.current.getRasterTileService({
-              queryParams: {
-                style: "explore.day",
-                size: 512,
-              },
+                queryParams: {
+                    style: "explore.day",
+                    size: 512,
+                },
             });
             // Creates a new instance of the H.service.rasterTile.Provider class
             // The class provides raster tiles for a given tile layer ID and pixel format
-            const rasterTileProvider = new H.service.rasterTile.Provider(
-              rasterTileService
-            );
+            const rasterTileProvider = new H.service.rasterTile.Provider(rasterTileService);
             // Create a new Tile layer with the Raster Tile provider
             const rasterTileLayer = new H.map.layer.TileLayer(rasterTileProvider);
             // Create a new map instance with the Tile layer, center and zoom level
@@ -41,89 +40,50 @@ const Map = ( props ) => {
                     zoom: 14,
                 },
             );
-            
-
 
             // Add panning and zooming behavior to the map
-            const behavior = new H.mapevents.Behavior(
-              new H.mapevents.MapEvents(newMap)
-            );
-      
+            const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(newMap));
+
             // Set the map object to the reference
             map.current = newMap;
             map.mapevents = behavior;
+
+            // Function to handle click on map
+            const handleMapClick = (event) => {
+                // Additional information to display
+                const additionalInfo = "Additional information goes here.";
+                // const additionalInfo = "Additional information goes here.";
+                const { lat, lng } = map.current.screenToGeo(event.currentPointer.viewportX, event.currentPointer.viewportY);
+                // alert(`Clicked at: \nLatitude ${lat}, Longitude ${lng}\n${additionalInfo}` );
+                setopen(true);
+                setLat(lat);
+                setLong(lng);
+            };
             
-            
-          }
-        },
-        // Dependencies array
-        [apikey, userPosition, restaurantPosition]
-      );
-      
-      // Return a div element to hold the map
-      return <div style={ { width: "100%", height: "500px" } } ref={mapRef} />;
-      
-   }
-   function getMarkerIcon(color) {
-    const svgCircle = `<svg width="20" height="20" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <g id="marker">
-                <circle cx="10" cy="10" r="7" fill="${color}" stroke="${color}" stroke-width="4" />
-                </g></svg>`;
-    return new H.map.Icon(svgCircle, {
-        anchor: {
-            x: 10,
-            y: 10
+
+            // Add tap event listener to the map
+            map.current.addEventListener('tap', handleMapClick);
+
         }
-    });
-}
-function calculateRoute(platform, map, start, destination) {
-    function routeResponseHandler(response) {
-        const sections = response.routes[0].sections;
-        const lineStrings = [];
-        sections.forEach((section) => {
-            // convert Flexible Polyline encoded string to geometry
-            lineStrings.push(H.geo.LineString.fromFlexiblePolyline(section.polyline));
-        });
-        const multiLineString = new H.geo.MultiLineString(lineStrings);
-        // const bounds = multiLineString.getBoundingBox();
+    }, [apikey, userPosition, restaurantPosition]);
 
-        // Create the polyline for the route
-        const routePolyline = new H.map.Polyline(multiLineString, {
-            style: {
-                lineWidth: 5
-            }
-        });
+    
 
-        // Remove all the previous map objects, if any
-        map.removeObjects(map.getObjects());
-        // Add the polyline to the map
-        map.addObject(routePolyline);
-        map.addObjects([
-            // Add a marker for the user
-            new H.map.Marker(start, {
-                icon: getMarkerIcon('red')
-            }),
-            // Add a marker for the selected restaurant
-            new H.map.Marker(destination, {
-                icon: getMarkerIcon('green')
-            })
-        ]);
-    }
-
-    // Get an instance of the H.service.RoutingService8 service
-    const router = platform.getRoutingService(null, 8);
-
-    // Define the routing service parameters
-    const routingParams = {
-        'origin': `${start.lat},${start.lng}`,
-        'destination': `${destination.lat},${destination.lng}`,
-        'transportMode': 'car',
-        'return': 'polyline'
-    };
-    // Call the routing service with the defined parameters
-    router.calculateRoute(routingParams, routeResponseHandler, console.error);
+    // Return a div element to hold the map
+    return  <div>
+    <div style={{ width: "100%", height :"700px" }} ref={mapRef} />  
+{open && (
+    <Modal
+    open={open}
+    onClose={() => { setopen(false)}}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+  >
+    <BasicModal lng={long} lat={lat}/>
+  </Modal>
+)}
+    </div>
+    
 }
 
-   
-  
-   export default Map;
+export default Map;
